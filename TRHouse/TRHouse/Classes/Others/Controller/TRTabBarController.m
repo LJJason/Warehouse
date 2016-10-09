6b4ba6dd3ad58c66814488238097262ed7191456
CoderGFWu <wgf@wgfdeMacBook-Air.local>
2016-10-09 14:33:51 +0800

    添加分类
    快速根据StoryboardName, identifier获取控制器的分类, 直接用view.x 拿到坐标的的UIView的分类

--- TRHouse/TRHouse/Classes/Others/Controller/TRTabBarController.m
+++ TRHouse/TRHouse/Classes/Others/Controller/TRTabBarController.m
@@ -40,7 +40,7 @@
     //互动
     [self addChildViewController:[[TRInteractiveTableViewController alloc] init] withTitle:@"互动" withImageName:@"tabbar_inter"];
     //发现
-    [self addChildViewController:[[TRFoundTableViewController alloc] init] withTitle:@"发现" withImageName:@"tabbar_found"];
+    [self addChildViewController:[TRFoundTableViewController instantiateInitialViewControllerWithStoryboardName:@"TRFound"] withTitle:@"发现" withImageName:@"tabbar_found"];
     //我
     [self addChildViewController:[[TRMeTableViewController alloc] init] withTitle:@"我" withImageName:@"tabbar_me"];
     
