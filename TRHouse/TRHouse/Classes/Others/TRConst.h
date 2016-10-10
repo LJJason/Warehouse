071b640c4185f073722f8083e2c6b0fbd53a60f3
CoderGFWu <wgf@wgfdeMacBook-Air.local>
2016-10-10 08:55:02 +0800

    添加常量包

---
+++ TRHouse/TRHouse/Classes/Others/TRConst.h
@@ -0,0 +1,13 @@
+//
+//  TRConst.h
+//  TRMerchants
+//
+//  Created by wgf on 16/10/8.
+//  Copyright © 2016年 wgf. All rights reserved.
+//  常量包
+
+#import <UIKit/UIKit.h>
+
+/*** 旅游攻略的接口 */
+UIKIT_EXTERN NSString * const TRTourUrl;
+
---
+++ TRHouse/TRHouse/Classes/Others/TRConst.m
@@ -0,0 +1,6 @@
+
+#import <UIKit/UIKit.h>
+
+/*** 旅游攻略的接口 */
+NSString * const TRTourUrl = @"http://www.lvmama.com/?losc=018582&tele=499&cm_mmc=baidu10-_-cpc-_-sem-_-pc&utm_source=baidu10&utm_medium=sem&utm_campaign=cpc";
+
