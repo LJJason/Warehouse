faa5ccbff5f975dc2847a4c7c45da0c1ad05a89a
admin1 <1573269370@qq.com>
2016-10-13 17:03:23 +0800

    临居圈
    临居圈的界面的搭建和初步数据的显示

---
+++ TRHouse/TRHouse/Classes/Found/Model/TRPost.m
@@ -0,0 +1,17 @@
+//
+//  TRPost.m
+//  TRHouse
+//
+//  Created by admin1 on 2016/10/13.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import "TRPost.h"
+
+@implementation TRPost
+
++ (NSDictionary *)mj_replacedKeyFromPropertyName{
+    return @{@"ID" : @"id"};
+}
+
+@end
---
+++ TRHouse/TRHouse/Classes/Found/View/TRPostTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TRPostTableViewCell.h
+//  TRHouse
+//
+//  Created by admin1 on 2016/10/13.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class TRPost;
+@interface TRPostTableViewCell : UITableViewCell
+
+@property (nonatomic,strong) TRPost *posts;
+
+@end
